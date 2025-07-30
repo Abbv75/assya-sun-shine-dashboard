6d2f5cc38c735a382888887b8f70036ab7b3a1a3
abbv75 <bore.younous59@gmail.com>
2025-07-30 17:23:05 +0000

    `Removed unused pages and components: SelectedPartie, SelectedTournoi, Tournoi`

--- src/constant/pagePath.ts
+++ src/constant/pagePath.ts
@@ -1,8 +1,5 @@
-import { faGamepad, faLock, faUserClock, faUsersGear } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faUsersGear } from "@fortawesome/free-solid-svg-icons";
 import User from "../pages/User";
-import Tournoi from "../pages/Tournoi";
-import SelectedTournoi from "../pages/SelectedTournoi";
-import SelectedPartie from "../pages/SelectedPartie";
 import Categorie from "../pages/Categorie";
 import { faStackOverflow } from "@fortawesome/free-brands-svg-icons";
 
@@ -20,29 +17,6 @@ export default [
         icon: faStackOverflow,
         component: Categorie,
         isPublic: false
-    },
-    {
-        label: 'Gestion des tournois',
-        href: '/tournoi',
-        icon: faGamepad,
-        component: Tournoi,
-        isPublic: false
-    },
-    {
-        label: 'Tournois selectionné',
-        href: '/tournoi-selectionne/:idTournoi',
-        icon: faUserClock,
-        component: SelectedTournoi,
-        isPublic: false,
-        toHide: true
-    },
-    {
-        label: 'Partie selectionnée',
-        href: '/partie-selectionne/:idTournoi/:idPartie',
-        icon: faUserClock,
-        component: SelectedPartie,
-        isPublic: false,
-        toHide: true
     },
     {
         label: 'Connexion',
