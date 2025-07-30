459901173e449f773ae54ba7528c53ab6f0cb3b4
abbv75 <bore.younous59@gmail.com>
2025-07-30 15:55:58 +0000

    `Removed unused images and updated connexion background image in constant/image.js`

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -57,7 +57,6 @@ const Navbar = () => {
                 <FontAwesomeIcon icon={faLock} />
             </Button>
         </Card>
-
     )
 }
 
--- src/constant/image.js
+++ src/constant/image.js
@@ -1,4 +1,4 @@
 export default {
     logo: '/logo.jpg',
-    connexionBg: '/assets/images/call-of-duty-mobile-hero-a.jpg',
+    connexionBg: '/assets/images/connexionbg.webp',
 }
